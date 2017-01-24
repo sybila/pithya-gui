772f2dd3a487958167312d9fc48eba8815f9b101
Samuel Pastva <daemontus@gmail.com>
2017-01-24 17:12:18 +0100

    remove packman

--- config.R
+++ config.R
@@ -1,8 +1,19 @@
 ### This is a global configuration file. Include this in all other files!
 
-# Load and install (if needed) all app dependencies using the packman manager.
-if (!require("pacman")) install.packages("pacman", quiet = F)
-pacman::p_load(methods, shiny, shinyBS, shinyAce, shinyjs, pracma, stringr, data.table, rjson, parallel, matrixcalc)
+require("parallel")
+if (!require("methods")) {install.packages("methods", quiet = F); library(methods)}
+if (!require("shiny")) {install.packages("shiny", quiet = F); library(shiny)}
+if (!require("shinyBS")) {install.packages("shinyBS", quiet = F); library(shinyBS)}
+if (!require("shinyjs")) {install.packages("shinyjs", quiet = F); library(shinyjs)}
+if (!require("shinyAce")) {install.packages("shinyAce", quiet = F);
+library(shinyAce)}
+if (!require("pracma")) {install.packages("pracma", quiet = F); library(pracma)}
+if (!require("stringr")) {install.packages("stringr", quiet = F); library(stringr)}
+if (!require("data.table")) {install.packages("data.table", quiet = F);
+library(data.table)}
+if (!require("rjson")) {install.packages("rjson", quiet = F); library(rjson)}
+if (!require("matrixcalc")) {install.packages("matrixcalc", quiet = F);
+library(matrixcalc)}
 
 source("global.R")
 source("util.R")
