c0fe6290f1dccf03525e7d1c0abb2a70a83469c5
Samuel Pastva <daemontus@gmail.com>
2017-07-24 09:28:03 +0200

    fix analytics

--- ui.R
+++ ui.R
@@ -32,18 +32,6 @@ Shiny.addCustomMessageHandler('scaleSliderHandler',
 "
 
 shinyUI(
-    tags$head(HTML("
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-10610329-11', 'auto');
-          ga('send', 'pageview');
-
-        </script>
-"))
     fluidPage(
         ### Working solution for re-loading of fileInput
         # tags$script('
@@ -54,6 +42,18 @@ shinyUI(
         #     $(id).css("visibility", "hidden");
         # });
         # '),
+        tags$head(HTML("
+                <script>
+                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                  ga('create', 'UA-10610329-11', 'auto');
+                  ga('send', 'pageview');
+
+                </script>
+        ")),
         useShinyjs(),
         # loading(file.path(mytempdir(), "image.Rmd")),
         # fluidPage(
