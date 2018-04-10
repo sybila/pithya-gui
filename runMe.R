039796a111b198778fe1976835bc6677f5f524f7
martindemko <325073@mail.muni.cz>
2018-04-10 20:19:45 +0000

    edits runMe script

--- runMe.R
+++ runMe.R
@@ -1,3 +1,3 @@
 chooseCRANmirror(graphics=FALSE, ind=19)
 source("config.R")
-shiny::runApp('/home/shiny/pithya-gui', launch.browser=FALSE, host="127.0.0.1", port=8080)
+shiny::runApp('./', launch.browser=FALSE, host="127.0.0.1", port=8090)
